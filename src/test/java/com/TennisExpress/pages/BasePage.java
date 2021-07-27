aa298b0a3b2a665ca88c247568930e67c78aff50
bradselcuk <bradselcuk@gmail.com>
2021-07-27 10:19:56 -0500

    Initial commit

---
+++ src/test/java/com/TennisExpress/pages/BasePage.java
@@ -0,0 +1,17 @@
+package com.TennisExpress.pages;
+
+import com.TennisExpress.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+    protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 20);
+
+   public BasePage(){
+       PageFactory.initElements(driver, this);
+   }
+}
